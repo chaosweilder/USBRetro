5016b98dbbc04033dd9e18f92bec169fddc6cd99
chaosweilder <timehero.final.anti.form@gmail.com>
2025-01-29 14:50:36 +0100

    Update gamecube.c

--- src/console/gamecube/gamecube.c
+++ src/console/gamecube/gamecube.c
@@ -273,12 +273,13 @@ void __not_in_flash_func(update_output)(void)
       gc_report.dpad_left  |= ((byte & USBR_BUTTON_DL) == 0) ? 1 : 0; // left
       gc_report.a          |= ((byte & USBR_BUTTON_B2) == 0) ? 1 : 0; // b
       gc_report.b          |= ((byte & USBR_BUTTON_B1) == 0) ? 1 : 0; // a
-      gc_report.z          |= ((byte & USBR_BUTTON_R1) == 0) ? 1 : 0; // select
+      gc_report.z          |= ((byte & USBR_BUTTON_R1) == 0) ? 1 : 0; // zr
       gc_report.start      |= ((byte & USBR_BUTTON_S2) == 0) ? 1 : 0; // start
       gc_report.x          |= ((byte & USBR_BUTTON_B4) == 0) ? 1 : 0; // y
       gc_report.y          |= ((byte & USBR_BUTTON_B3) == 0) ? 1 : 0; // x
-      gc_report.l          |= ((byte & USBR_BUTTON_L1) == 0) ? 1 : 0; // l
+      gc_report.z          |= ((byte & USBR_BUTTON_L1) == 0) ? 1 : 0; // zl
       gc_report.r          |= ((byte & USBR_BUTTON_R2) == 0) ? 1 : 0; // r
+      gc_report.l          |= ((byte & USBR_BUTTON_L2) == 0) ? 1 : 0; // l
 
       // global dominate axis
       gc_report.stick_x    = furthest_from_center(gc_report.stick_x, players[i].output_analog_1x, 128);
@@ -370,13 +371,13 @@ void __not_in_flash_func(post_globals)(
       players[player_index].output_buttons &= ~0x200;
     }
 
-    if (!((players[player_index].output_buttons) & 0x4000))
+    if (!((players[player_index].output_buttons) & 0x100))
     {
       players[player_index].output_analog_l = 255;
     }
     else if (analog_l > 250)
     {
-      players[player_index].output_buttons &= ~0x4000;
+      players[player_index].output_buttons &= ~0x100;
     }
 
     // printf("X1: %d, Y1: %d   ", analog_1x, analog_1y);
