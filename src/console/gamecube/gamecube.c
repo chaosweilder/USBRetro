e71ab2d9d71ec047e20d130dcd7065997b3f6c57
chaosweilder <timehero.final.anti.form@gmail.com>
2025-01-29 14:32:27 +0100

    Update gamecube.c

--- src/console/gamecube/gamecube.c
+++ src/console/gamecube/gamecube.c
@@ -273,12 +273,12 @@ void __not_in_flash_func(update_output)(void)
       gc_report.dpad_left  |= ((byte & USBR_BUTTON_DL) == 0) ? 1 : 0; // left
       gc_report.a          |= ((byte & USBR_BUTTON_B2) == 0) ? 1 : 0; // b
       gc_report.b          |= ((byte & USBR_BUTTON_B1) == 0) ? 1 : 0; // a
-      gc_report.z          |= ((byte & USBR_BUTTON_S1) == 0) ? 1 : 0; // select
+      gc_report.z          |= ((byte & USBR_BUTTON_R1) == 0) ? 1 : 0; // select
       gc_report.start      |= ((byte & USBR_BUTTON_S2) == 0) ? 1 : 0; // start
       gc_report.x          |= ((byte & USBR_BUTTON_B4) == 0) ? 1 : 0; // y
       gc_report.y          |= ((byte & USBR_BUTTON_B3) == 0) ? 1 : 0; // x
       gc_report.l          |= ((byte & USBR_BUTTON_L1) == 0) ? 1 : 0; // l
-      gc_report.r          |= ((byte & USBR_BUTTON_R1) == 0) ? 1 : 0; // r
+      //gc_report.r          |= ((byte & USBR_BUTTON_R1) == 0) ? 1 : 0; // r
 
       // global dominate axis
       gc_report.stick_x    = furthest_from_center(gc_report.stick_x, players[i].output_analog_1x, 128);
