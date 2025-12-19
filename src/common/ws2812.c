0bf6b2c2a7372cb880dcd853defeb76b0e40087e
chaosweilder <timehero.final.anti.form@gmail.com>
2025-12-19 18:55:46 +0100

    Update ws2812.c

--- src/common/ws2812.c
+++ src/common/ws2812.c
@@ -13,7 +13,7 @@
 #define NUM_PIXELS 1
 
 #ifdef PICO_DEFAULT_WS2812_PIN
-#define WS2812_PIN PICO_DEFAULT_WS2812_PIN
+#define WS2812_PIN 16
 #else
 // default to pin 2 if the board doesn't have a default WS2812 pin defined
 #define WS2812_PIN 2
