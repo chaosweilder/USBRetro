5fd3644cc9e49bebc44e06d56066b1ab45025ab8
chaosweilder <timehero.final.anti.form@gmail.com>
2026-06-17 19:09:14 +0200

    Update app.h

--- src/apps/usb2neogeo/app.h
+++ src/apps/usb2neogeo/app.h
@@ -79,6 +79,19 @@
     #define P1_NEOGEO_B4_PIN 4
     #define P1_NEOGEO_B5_PIN 9
     #define P1_NEOGEO_B6_PIN 2
+#elif defined(PICO_RP2350_CORE_BUILD)
+    #define P1_NEOGEO_DU_PIN 14
+    #define P1_NEOGEO_DD_PIN 29
+    #define P1_NEOGEO_DR_PIN 28
+    #define P1_NEOGEO_DL_PIN 13
+    #define P1_NEOGEO_S1_PIN 3
+    #define P1_NEOGEO_S2_PIN 10
+    #define P1_NEOGEO_B1_PIN 12
+    #define P1_NEOGEO_B2_PIN 27
+    #define P1_NEOGEO_B3_PIN 11
+    #define P1_NEOGEO_B4_PIN 4
+    #define P1_NEOGEO_B5_PIN 9
+    #define P1_NEOGEO_B6_PIN 2
 #else
     #define P1_NEOGEO_DU_PIN 29
     #define P1_NEOGEO_DD_PIN 2
@@ -103,6 +116,8 @@
     #define BOARD "pico"
 #elif defined(PICO_RP2040_ZERO_BUILD)
     #define BOARD "rp2040zero"
+#elif defined(PICO_RP2350_CORE_BUILD)
+    #define BOARD "rp2350core"
 #else
     #define BOARD "ada_kb2040"
 #endif
