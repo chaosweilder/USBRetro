f4546223ff690a55efbd724840a9163f7a2a7a77
chaosweilder <timehero.final.anti.form@gmail.com>
2026-06-22 23:00:11 +0200

    Update pcengine_device.h

--- src/native/device/pcengine/pcengine_device.h
+++ src/native/device/pcengine/pcengine_device.h
@@ -18,16 +18,26 @@
 #define MAX_PLAYERS 5               // PCE supports up to 5 players
 
 // Defaults to ADAFRUIT_KB2040, build for Adafruit KB2040 board
-#define DATAIN_PIN  14
-#define CLKIN_PIN   DATAIN_PIN + 1  // Note - in pins must be a consecutive 'in' group
+
 
 #ifdef RPI_PICO_BUILD
+  #define DATAIN_PIN  18
+  #define CLKIN_PIN   DATAIN_PIN + 1  // Note - in pins must be a consecutive 'in' group
+  #define OUTD0_PIN   4
+  #define OUTD1_PIN   5
+  #define OUTD2_PIN   6
+  #define OUTD3_PIN   7
+#elif defined(PICO_RP2040_ZERO_BUILD)
+  #define DATAIN_PIN  14
+  #define CLKIN_PIN   DATAIN_PIN + 1  // Note - in pins must be a consecutive 'in' group
   #define OUTD0_PIN   4
   #define OUTD1_PIN   5
   #define OUTD2_PIN   6
   #define OUTD3_PIN   7
 #else
   // Default configuration
+  #define DATAIN_PIN  18
+  #define CLKIN_PIN   DATAIN_PIN + 1  // Note - in pins must be a consecutive 'in' group
   #define OUTD0_PIN   26 // Note - out pins must be a consecutive 'out' group
   #define OUTD1_PIN   27
   #define OUTD2_PIN   28
