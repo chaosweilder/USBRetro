e3f92225e62afe9b9a310814b97f303ed9ba619c
chaosweilder <timehero.final.anti.form@gmail.com>
2026-06-23 11:06:55 +0200

    Update pcengine_device.h

--- src/native/device/pcengine/pcengine_device.h
+++ src/native/device/pcengine/pcengine_device.h
@@ -35,8 +35,6 @@
   #define OUTD3_PIN   29
 #else
   // Default configuration
-  #define DATAIN_PIN  18
-  #define CLKIN_PIN   DATAIN_PIN + 1  // Note - in pins must be a consecutive 'in' group
   #define OUTD0_PIN   26 // Note - out pins must be a consecutive 'out' group
   #define OUTD1_PIN   27
   #define OUTD2_PIN   28
